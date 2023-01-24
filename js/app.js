21eba9de0ebf494710a871d9052d3d34574ad446
Authoi234 <jawadkhan.authoi@gmail.com>
2023-01-24 17:17:11 +0600

    modal complete

--- js/app.js
+++ js/app.js
@@ -37,6 +37,7 @@ const displayPhones = (phones, dataLimit) => {
                 <h5 class="card-title">${phone.phone_name}</h5>
                 <p class="card-text">This is a longer card with supporting text below as a natural
                 lead-in to additional content. This content is a little bit longer.</p>
+                <button onclick="loadPhoneDetails('${phone.slug}')" href="#" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#phoneDetailModal">Show Details</button>
             </div>
         </div>
         `
@@ -59,6 +60,14 @@ document.getElementById('btn-search').addEventListener('click', function(){
     processSearch(10)
 })
 
+// search input field enter handler
+document.getElementById('search-field').addEventListener('keypress', function (e) {
+    console.log(e.key);
+    if (e.key === 'Enter') {
+        processSearch(10)
+    }
+});
+
 const toggleSpinner = isLoading => {
     const loaderSection = document.getElementById('loader')
     if (isLoading) {
@@ -73,3 +82,23 @@ const toggleSpinner = isLoading => {
 document.getElementById('btn-show-all').addEventListener('click', function(){
     processSearch()
 })
+
+const loadPhoneDetails = async id => {
+    const url = `https://openapi.programming-hero.com/api/phone/${id}`
+    const res = await fetch(url)
+    const data = await res.json()
+    displayPhoneDetails(data.data);
+}
+
+const displayPhoneDetails = phone => {
+    const modalTitle = document.getElementById('phoneDetailModalLabel')
+    modalTitle.innerText = phone.brand + ' | ' + phone.name;
+    const phoneDetails = document.getElementById('phone-details')
+    phoneDetails.innerHTML = `
+    <img src="${phone.image}" alt="">
+    <p>Release Date: ${phone.releaseDate ? phone.releaseDate : 'No release date found'}</p>
+    <p>Storage: ${phone.mainFeatures ? phone.mainFeatures.storage : 'No storage information found'}</p>
+    <p>Bluetooth: ${phone.others ? phone.others.Bluetooth : 'No bluetooth information'}</p>
+    `
+    console.log(phone);
+}
